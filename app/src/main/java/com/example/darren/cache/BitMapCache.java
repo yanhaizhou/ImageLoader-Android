527e6ab76e49ca44c7b318be16f99263d15d730d
Darren <274965301@qq.com>
2016-02-19 19:02:25 +0800

    第2次测试更新

---
+++ app/src/main/java/com/example/darren/cache/BitMapCache.java
@@ -0,0 +1,11 @@
+package com.example.darren.cache;
+
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+
+/**
+ * Created by Darren on 16/1/23.
+ */
+public interface BitMapCache {
+    AlertDialog
+}
---
+++ app/src/main/java/com/example/darren/config/ImageLoaderConfig.java
@@ -0,0 +1,15 @@
+package com.example.darren.config;
+
+/**
+ * Created by Darren on 16/1/23.
+ */
+
+import android.graphics.Bitmap;
+
+/**
+ * ImageLoader配置类
+ */
+public class ImageLoaderConfig {
+
+   //public BitMapCache bitMapCache = new MemoryCache();
+}
